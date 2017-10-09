96a9870753aecb2716e247a523baf83b2dd49f5e
Anthony <Anthony@Anthonys-iMac.local>
2017-10-08 18:10:53 -0700

    Test Class + Chrome

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,39 @@
 from selenium import webdriver
+import unittest
 
 browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+
+class SiteIsUp(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+
+    def tearDown(self):
+        browser.quit()
+
+    def test_site_is_running(self):
+        # User visits awesome new annotations website's home page
+        browser.get('http://localhost:8000')
+
+        # User sees the site name 'White Wall' in the title of the running application
+        self.assertIn('Api', self.browser.title)
+        self.fail('Finish the test!')
+
+        # User sees login as the root page
+
+        # User can navigate to 'sign up'
+
+        # User can Login and are taken to the in app control window
+
+        # User can user the in app browser to navigate the interenet
+
+        # User can select annotations and read their contents
+
+        # User can upload answers to annotations
+
+        # User can click answer links
+
+        # User can view article summary
+
+if __name__ == '__main__':
+    unittest.main()
