b7a59b6c1d01ddba154bf71a3a53eeddbcca1f66
irvinemd55 <irvinedowning@gmail.com>
2017-10-09 16:09:23 -0700

    basic template added

--- apps/dressing/urls.py
+++ apps/dressing/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^', views.index, name='welcome') 
+    url(r'^', views.index, name='welcome'),
+    url(r'^show/(?P<num>[0-9]+)', views.choose_article, name='choose_article'), 
 ]
