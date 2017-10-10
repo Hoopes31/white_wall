f7e9e47649560ea55f82fc3972b840ace0ad3333
Travis Alcantara <travis.alcantara@gmail.com>
2017-10-10 14:13:40 -0700

    finished views

---
+++ apps/dressing/pages_dictionary.py
@@ -0,0 +1,34 @@
+pages = [
+    {
+        'heading': 'Django REST Home',
+        'url': 'http://www.django-rest-framework.org/'
+    },
+    {
+        'heading': 'Serialization',
+        'url': 'http://www.django-rest-framework.org/tutorial/1-serialization/',
+    },
+    {
+        'heading': 'Requests and Responses',
+        'url': 'http://www.django-rest-framework.org/tutorial/2-requests-and-responses/',
+    },
+    {
+        'heading': 'Class Based Views',
+        'url': 'http://www.django-rest-framework.org/tutorial/3-class-based-views/',
+    },
+    {   
+        'heading': 'Authentication and Permissions',
+        'url': 'http://www.django-rest-framework.org/tutorial/4-authentication-and-permissions/',
+    },
+    {
+        'heading': 'Relationships & Hyperlinked APIs',
+        'url': 'http://www.django-rest-framework.org/tutorial/5-relationships-and-hyperlinked-apis/',
+    },
+    {
+        'heading': 'Viewsets and Routers',
+        'url': 'http://www.django-rest-framework.org/tutorial/6-viewsets-and-routers/',
+    },
+    {
+        'heading': 'Schemas & Client Libraries',
+        'url': 'http://www.django-rest-framework.org/tutorial/7-schemas-and-client-libraries/',
+    },
+]
--- apps/dressing/urls.py
+++ apps/dressing/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='welcome'),
-    url(r'^white_wall', views.white_wall, name='white_wall'),
-    url(r'^show/(?P<num>[0-9]+)', views.choose_article, name='choose_article'), 
-]
+    url(r'^show/(?P<article_id>\d+)', views.show, name='show'), 
+    url(r'^get_or_create', views.get_or_create_article, name='get_or_create_article'),
+ ]
